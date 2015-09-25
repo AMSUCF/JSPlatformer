eb4e1e8f31284842c3e998f5bde3212eed226634
AMSUCF <anastasia@ucf.edu>
2015-09-25 19:39:59 -0400

    Clip edges using css and add control of window via arrow keys

--- code/game.js
+++ code/game.js
@@ -34,6 +34,20 @@ function Level(plan) {
   }
 }
 
+function Vector(x, y) {
+  this.x = x; this.y = y;
+}
+
+// Vector arithmetic: v_1 + v_2 = <a,b>+<c,d> = <a+c,b+d>
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+// Vector arithmetic: v_1 * factor = <a,b>*factor = <a*factor,b*factor>
+Vector.prototype.times = function(factor) {
+  return new Vector(this.x * factor, this.y * factor);
+};
+
 // Helper function to easily create an element of a type provided 
 // and assign it a class.
 function elt(name, className) {
@@ -48,6 +62,10 @@ function DOMDisplay(parent, level) {
 // this.wrap corresponds to a div created with class of "game"
   this.wrap = parent.appendChild(elt("div", "game"));
   this.level = level;
+
+  // We are keeping track of the frame through these two properties
+  this.pos = new Vector(0, -0.5);
+  this.speed = new Vector(0,0);
 
   // In this version, we only have a static background.
   this.wrap.appendChild(this.drawBackground());
@@ -71,9 +89,101 @@ DOMDisplay.prototype.drawBackground = function() {
   return table;
 };
 
+// Hand-tuned values to determine scroll speed for the screen 
+
+var screenXSpeed = 100;
+var screenYSpeed = 100;
+
+// Scrolls the viewport by using arrow keys
+DOMDisplay.prototype.scrollView = function(keys, step) {
+  // Adding in arrow keys to scroll across level
+  var width = this.wrap.clientWidth;
+  var maxWidth = this.wrap.scrollWidth;
+  var height = this.wrap.clientHeight;
+  var maxHeight = this.wrap.scrollHeight;
+
+  // Adding in arrow keys to scroll across level
+
+  this.speed.x = 0;
+  // Set speed to 0 if we are off either side of screen, otherwise increase by arrow key and speed
+  if (keys.left && this.pos.x > 0) this.speed.x -= screenXSpeed;
+  if (keys.right && (this.pos.x < (maxWidth-width))) this.speed.x += screenXSpeed;
+
+  // Do same for y value.
+  this.speed.y = 0;
+  if (keys.up && this.pos.y > 0) this.speed.y -= screenYSpeed;
+  if (keys.down && (this.pos.y < (maxHeight-height))) this.speed.y += screenYSpeed;
+
+  // Calculate smooth motion using the step interval (time since last frame)
+  var motion = new Vector(this.speed.x * step, this.speed.y * step);
+  var newPos = this.pos.plus(motion);
+
+  // scrollLeft and scrollTop determine which offsets to apply to the clipped element
+  this.wrap.scrollLeft = newPos.x;
+  this.wrap.scrollTop = newPos.y;
+
+  // Remember the position
+  this.pos = newPos;
+};
+
+// Arrow key codes for readibility
+var arrowCodes = {37: "left", 38: "up", 39: "right", 40: "down"};
+
+// Translate the codes pressed from a key event
+function trackKeys(codes) {
+  var pressed = Object.create(null);
+
+  // alters the current "pressed" array which is returned from this function. 
+  // The "pressed" variable persists even after this function terminates
+  // That is why we needed to assign it using "Object.create()" as 
+  // otherwise it would be garbage collected
+
+  function handler(event) {
+    if (codes.hasOwnProperty(event.keyCode)) {
+      // If the event is keydown, set down to true. Else set to false.
+      var down = event.type == "keydown";
+      pressed[codes[event.keyCode]] = down;
+      // We don't want the key press to scroll the browser window, 
+      // This stops the event from continuing to be processed
+      event.preventDefault();
+    }
+  }
+  addEventListener("keydown", handler);
+  addEventListener("keyup", handler);
+  return pressed;
+}
+
+// frameFunc is a function called each frame with the parameter "step"
+// step is the amount of time since the last call used for animation
+function runAnimation(frameFunc) {
+  var lastTime = null;
+  function frame(time) {
+    var stop = false;
+    if (lastTime != null) {
+      // Set a maximum frame step of 100 milliseconds to prevent
+      // having big jumps
+      var timeStep = Math.min(time - lastTime, 100) / 1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (!stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+// This assigns the array that will be updated anytime the player
+// presses an arrow key. We can access it from anywhere.
+var arrows = trackKeys(arrowCodes);
+
 // Organize a single level and begin animation
 function runLevel(level, Display) {
   var display = new Display(document.body, level);
+
+  runAnimation(function(step) {
+    // Allow the viewer to scroll the level
+    display.scrollView(arrows,step);
+  });
 }
 
 function runGame(plans, Display) {
